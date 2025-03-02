5737e3e398c6a143aa7df52749bbbf453e7e615b
Ruly Rahman <d.ferdiansya@gmail.com>
2025-03-03 03:44:19 +0700

    initial commit

---
+++ src/main/java/invensa/jav/ItemTransactionApplication.java
@@ -0,0 +1,13 @@
+package invensa.jav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/invensa/jav/ItemTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package invensa.jav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
